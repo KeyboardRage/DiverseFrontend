df918a8a48db50a09061c8850f5ea0f00bb97f80
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2020-03-11 15:11:31 +0200

    more fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { NavbarComponent } from './home-page/navbar/navbar.component';
-import { GuildFeaturesComponent } from './home-page/guild-features/guild-features.component';
-import { InviteModuleComponent } from './home-page/invite-module/invite-module.component';
 import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    NavbarComponent,
-    GuildFeaturesComponent,
-    InviteModuleComponent
     LandingComponent
   ],
   imports: [
