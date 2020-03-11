30b0f0769c5d32430d6e30844ad1b0671da978a9
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2020-03-11 18:08:31 +0200

    Created landing page

--- src/app/landing/components/navbar/navbar.component.spec.ts
+++ src/app/landing/components/navbar/navbar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LandingComponent } from './landing.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('LandingComponent', () => {
-  let component: LandingComponent;
-  let fixture: ComponentFixture<LandingComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
+      declarations: [ NavbarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LandingComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  features = [
+    {
+      icon: "fas fa-comments",
+      caption: "Feedback channels where you can post your work, and get feedback from designers and non-designers."
+    },
+    {
+      icon: "fas fa-question",
+      caption: "Technical help channel, in case you need help with one of the softwares, drawing tablet, theory, and so on."
+    },
+    {
+      icon: "fas fa-shopping-basket",
+      caption: "Marketplace, where you can both offer your design services, or list them on a job board. We also have a system for reviews."
+    },
+    {
+      icon: "fas fa-robot",
+      caption: "We use a proprietary bot, in addition to some popular powerful ones, that is under constant development to enable the guild to have unique features, such as raffle, perks, AI powered FAQ, quick help, and more."
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
