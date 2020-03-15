1a1e21f7b6b41f27b81cebc9773ec16dce56c841
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2020-03-15 02:02:11 +0200

    lots of stuff done!

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -9,20 +9,16 @@ export class LandingComponent implements OnInit {
 
   features = [
     {
-      icon: "feedback.png",
-      caption: "Feedback channels where you can post your work, and get feedback from designers and non-designers."
+      icon: "ai.svg",
+      caption: "Dedicated help channel for getting technical help with specific programs or general design theory."
     },
     {
-      icon: "problem.png",
-      caption: "Technical help channel, in case you need help with one of the softwares, drawing tablet, theory, and so on."
+      icon: "lamp.svg",
+      caption: "Channels where you can post your work and get constructive feedback, ideas, or learn new things."
     },
     {
-      icon: "fas fa-shopping-basket",
-      caption: "Marketplace, where you can both offer your design services, or list them on a job board. We also have a system for reviews."
-    },
-    {
-      icon: "fas fa-robot",
-      caption: "We use a proprietary bot, in addition to some popular powerful ones, that is under constant development to enable the guild to have unique features, such as raffle, perks, AI powered FAQ, quick help, and more."
+      icon: "note.svg",
+      caption: "Channel to list graphic design requests, and channels to advertise your services, topped of with a review system."
     }
   ]
 
