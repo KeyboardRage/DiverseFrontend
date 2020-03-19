67efd722ae9cb7fff2d33e78b7b3592d9a9fdfb6
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2020-03-19 11:04:37 +0200

    dist

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { filter } from "rxjs/operators";
+import { filter, retry } from "rxjs/operators";
 
 @Pipe({
   name: "filter"
@@ -11,25 +11,26 @@ export class FilterPipe implements PipeTransform {
 
     searchTerm = searchTerm.toLowerCase();
     const result = items.filter(item => {
-      const userFound = item.user.includes(searchTerm);
-
-      const caseFound = item.case.toString() == searchTerm;
-      const usernameFound = item.user_tag.includes(searchTerm);
-      const creatorFound = item.creator.includes(searchTerm);
-      const creatornameFound = item.creator_tag
-        .toLowerCase()
-        .includes(searchTerm);
-      const textFound = item.data[0].text.includes(searchTerm);
-
-      return (
-        userFound ||
-        creatorFound ||
-        usernameFound ||
-        textFound ||
-        creatornameFound ||
-        caseFound
-      );
+      return item.case.toString() == searchTerm;
     });
+    if (result.length === 0) {
+      const result = items.filter(item => {
+        const usernameFound = item.user_tag.includes(searchTerm);
+        const creatorFound = item.creator.includes(searchTerm);
+        const creatornameFound = item.creator_tag
+          .toLowerCase()
+          .includes(searchTerm);
+        return usernameFound || creatorFound || creatornameFound;
+      });
+      if (result.length === 0) {
+        const result = items.filter(item => {
+          return item.data[0].text.includes(searchTerm);
+        });
+        return result;
+      }
+      return result;
+    }
+
     return result;
   }
 }
