73ec1d74f2f29c482523345df7214c7bf54f9e8d
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2020-05-21 02:04:56 +0300

    Hidden unused components and made click area smaller

--- src/app/admin-panel/components/table/table.component.ts
+++ src/app/admin-panel/components/table/table.component.ts
@@ -3,10 +3,23 @@ import { Component, OnInit, Input } from "@angular/core";
 @Component({
   selector: "app-table",
   templateUrl: "./table.component.html",
-  styleUrls: ["./table.component.scss"]
+  styleUrls: ["./table.component.scss"],
 })
 export class TableComponent {
   searchText;
 
   @Input() cases$;
+
+  mouseOver(obj) {
+    let parentNode = obj.target.parentElement;
+    parentNode.style.bottom = "5px";
+    parentNode.style.boxShadow = "0 4px 4px rgba(0, 0, 0, .25)";
+    parentNode.style.transition = " 0.2s all";
+    parentNode.style.borderRadius = "5px";
+    parentNode.style.backgroundColor = "#34343E";
+  }
+  mouseOff(obj) {
+    let parentNode = obj.target.parentElement;
+    parentNode.style = null;
+  }
 }
--- src/app/services/cases.service.ts
+++ src/app/services/cases.service.ts
@@ -2,10 +2,21 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: "root"
+  providedIn: "root",
 })
 export class CasesService {
   constructor(private http: HttpClient) {}
+  flags = {
+    active: 1,
+    note: 2,
+    warn: 4,
+    mute: 8,
+    dwc: 16,
+    kick: 32,
+    tempban: 64,
+    ban: 128,
+    archived: 256,
+  };
   showCase(id) {
     this.getCase(id).subscribe((res: any) => {
       console.log(res.data);
@@ -16,7 +27,12 @@ export class CasesService {
   }
 
   getCases() {
-    return this.http.get("https://api.diverse.graphics/public/cases/");
+    let flags =
+      Object.values(this.flags).reduce((a, b) => a | b) ^ this.flags.ban;
+
+    return this.http.get(
+      `https://api.diverse.graphics/public/cases?flags=${flags}&mode=and`
+    );
   }
   getBans() {
     return this.http.get("https://api.diverse.graphics/public/cases?flags=128");
